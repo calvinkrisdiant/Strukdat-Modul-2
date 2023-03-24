e6375071febd55268b0ecf7cfde7ee7fe2032209
calvinkrisdiant <127312880+calvinkrisdiant@users.noreply.github.com>
2023-03-24 15:08:41 +0700

    Add files via upload

--- Modul2StrukturData/src/main/java/com/mycompany/modul2strukturdata/linkedListHewan.java
+++ Modul2StrukturData/src/main/java/com/mycompany/modul2strukturdata/linkedListHewan.java
@@ -4,17 +4,49 @@
  */
 package com.mycompany.modul2strukturdata;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  *
  * @author asus
  */
 public class linkedListHewan {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    ArrayList<String> namaHewan = new ArrayList<>(
+            List.of("Sapi", "Kelinci", "Kambing", "Unta", "Domba")
+    );
+
+    public void hapusHewan(String del) {
+        namaHewan.remove(del);
     }
-    
+
+    public void tambahHewan(String add) {
+        namaHewan.add(add);
+    }
+
+    public void tampilHewan() {
+        for (String hewan : namaHewan) {
+            System.out.print(hewan + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        linkedListHewan linkedList = new linkedListHewan();
+        linkedList.tampilHewan();
+
+        Scanner scn = new Scanner(System.in);
+        System.out.print("\nMasukkan nama hewan yang ditambah: ");
+        String addHewan = scn.nextLine();
+
+        linkedList.tambahHewan(addHewan);
+        linkedList.tampilHewan();
+
+        System.out.print("\nMasukkan nama hewan yang dihapus: ");
+        String delHewan = scn.nextLine();
+
+        linkedList.hapusHewan(delHewan);
+        linkedList.tampilHewan();
+    }
 }
